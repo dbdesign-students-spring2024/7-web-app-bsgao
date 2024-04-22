b047a2e6ebf5090ba7e43b4743dc2d3573ee2bb2
bsgao <brandongao2003@gmail.com>
2024-04-21 20:04:43 -0400

    update

--- app.py
+++ app.py
@@ -4,189 +4,74 @@ import os
 import sys
 import subprocess
 import datetime
-
 from flask import Flask, render_template, request, redirect, url_for, make_response
-
-# import logging
-
-
- # delete this if not using sentry.io
-
-# from markupsafe import escape
-import pymongo
-from pymongo.errors import ConnectionFailure
+from pymongo import MongoClient, errors
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
 
-# load credentials and configuration options from .env file
-# if you do not yet have a file named .env, make one based on the template in env.example
-load_dotenv(override=True)  # take environment variables from .env.
+# Load environment variables from .env file
+load_dotenv(override=True)
 
-# initialize Sentry for help debugging... this requires an account on sentrio.io
-# you will need to set the SENTRY_DSN environment variable to the value provided by Sentry
-# delete this if not using sentry.io
-
-
-# instantiate the app using sentry for debugging
 app = Flask(__name__)
+app.config['DEBUG'] = os.getenv("FLASK_ENV", "development") == "development"
 
-# # turn on debugging if in development mode
-# app.debug = True if os.getenv("FLASK_ENV", "development") == "development" else False
+# Setup MongoDB connection from environment variables
+mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+mongo_dbname = os.getenv("MONGO_DBNAME", "mydatabase")
 
-# try to connect to the database, and quit if it doesn't work
 try:
-    cxn = pymongo.MongoClient(os.getenv("MONGO_URI"))
-    db = cxn[os.getenv("MONGO_DBNAME")]  # store a reference to the selected database
-
-    # verify the connection works by pinging the database
-    cxn.admin.command("ping")  # The ping command is cheap and does not require auth.
-    print(" * Connected to MongoDB!")  # if we get here, the connection worked!
-except ConnectionFailure as e:
-    # catch any database errors
-    # the ping command failed, so the connection is not available.
-    print(" * MongoDB connection error:", e)  # debug
-     # send the error to sentry.io. delete if not using
-    sys.exit(1)  # this is a catastrophic error, so no reason to continue to live
-
-
-# set up the routes
-
+    client = MongoClient(mongo_uri)
+    db = client[mongo_dbname]
+    print(" * Connected to MongoDB!")
+except errors.ConnectionFailure as e:
+    print(" * MongoDB connection error:", e)
+    sys.exit(1)
 
 @app.route("/")
 def home():
-    """
-    Route for the home page.
-    Simply returns to the browser the content of the index.html file located in the templates folder.
-    """
-    return render_template("index.html")
+    posts = db.posts.find().sort("created_at", -1)
+    return render_template("home.html", posts=posts)
 
+@app.route("/post/<post_id>")
+def post(post_id):
+    post = db.posts.find_one({"_id": ObjectId(post_id)})
+    return render_template("post.html", post=post)
 
-@app.route("/read")
-def read():
-    """
-    Route for GET requests to the read page.
-    Displays some information for the user with links to other pages.
-    """
-    docs = db.exampleapp.find({}).sort(
-        "created_at", -1
-    )  # sort in descending order of created_at timestamp
-    return render_template("read.html", docs=docs)  # render the read template
-
-
-@app.route("/create")
+@app.route("/create", methods=['GET', 'POST'])
 def create():
-    """
-    Route for GET requests to the create page.
-    Displays a form users can fill out to create a new document.
-    """
-    return render_template("create.html")  # render the create template
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        post = {
+            "title": title,
+            "content": content,
+            "created_at": datetime.datetime.utcnow()
+        }
+        db.posts.insert_one(post)
+        return redirect(url_for("home"))
+    return render_template("create.html")
 
+@app.route("/edit/<post_id>", methods=['GET', 'POST'])
+def edit(post_id):
+    post = db.posts.find_one({"_id": ObjectId(post_id)})
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        db.posts.update_one(
+            {"_id": ObjectId(post_id)},
+            {"$set": {"title": title, "content": content, "created_at": datetime.datetime.utcnow()}}
+        )
+        return redirect(url_for("post", post_id=post_id))
+    return render_template("edit.html", post=post)
 
-@app.route("/create", methods=["POST"])
-def create_post():
-    """
-    Route for POST requests to the create page.
-    Accepts the form submission data for a new document and saves the document to the database.
-    """
-    name = request.form["fname"]
-    message = request.form["fmessage"]
-
-    # create a new document with the data the user entered
-    doc = {"name": name, "message": message, "created_at": datetime.datetime.utcnow()}
-    db.exampleapp.insert_one(doc)  # insert a new document
-
-    return redirect(
-        url_for("read")
-    )  # tell the browser to make a request for the /read route
-
-
-@app.route("/edit/<mongoid>")
-def edit(mongoid):
-    """
-    Route for GET requests to the edit page.
-    Displays a form users can fill out to edit an existing record.
-
-    Parameters:
-    mongoid (str): The MongoDB ObjectId of the record to be edited.
-    """
-    doc = db.exampleapp.find_one({"_id": ObjectId(mongoid)})
-    return render_template(
-        "edit.html", mongoid=mongoid, doc=doc
-    )  # render the edit template
-
-
-@app.route("/edit/<mongoid>", methods=["POST"])
-def edit_post(mongoid):
-    """
-    Route for POST requests to the edit page.
-    Accepts the form submission data for the specified document and updates the document in the database.
-
-    Parameters:
-    mongoid (str): The MongoDB ObjectId of the record to be edited.
-    """
-    name = request.form["fname"]
-    message = request.form["fmessage"]
-
-    doc = {
-        # "_id": ObjectId(mongoid),
-        "name": name,
-        "message": message,
-        "created_at": datetime.datetime.utcnow(),
-    }
-
-    db.exampleapp.update_one(
-        {"_id": ObjectId(mongoid)}, {"$set": doc}  # match criteria
-    )
-
-    return redirect(
-        url_for("read")
-    )  # tell the browser to make a request for the /read route
-
-
-@app.route("/delete/<mongoid>")
-def delete(mongoid):
-    """
-    Route for GET requests to the delete page.
-    Deletes the specified record from the database, and then redirects the browser to the read page.
-
-    Parameters:
-    mongoid (str): The MongoDB ObjectId of the record to be deleted.
-    """
-    db.exampleapp.delete_one({"_id": ObjectId(mongoid)})
-    return redirect(
-        url_for("read")
-    )  # tell the web browser to make a request for the /read route.
-
-
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    """
-    GitHub can be configured such that each time a push is made to a repository, GitHub will make a request to a particular web URL... this is called a webhook.
-    This function is set up such that if the /webhook route is requested, Python will execute a git pull command from the command line to update this app's codebase.
-    You will need to configure your own repository to have a webhook that requests this route in GitHub's settings.
-    Note that this webhook does do any verification that the request is coming from GitHub... this should be added in a production environment.
-    """
-    # run a git pull command
-    process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
-    pull_output = process.communicate()[0]
-    # pull_output = str(pull_output).strip() # remove whitespace
-    process = subprocess.Popen(["chmod", "a+x", "flask.cgi"], stdout=subprocess.PIPE)
-    chmod_output = process.communicate()[0]
-    # send a success response
-    response = make_response(f"output: {pull_output}", 200)
-    response.mimetype = "text/plain"
-    return response
-
+@app.route("/delete/<post_id>")
+def delete(post_id):
+    db.posts.delete_one({"_id": ObjectId(post_id)})
+    return redirect(url_for("home"))
 
 @app.errorhandler(Exception)
 def handle_error(e):
-    """
-    Output any errors - good for debugging.
-    """
-    return render_template("error.html", error=e)  # render the edit template
+    return render_template("error.html", error=e)
 
-
-# run the app
 if __name__ == "__main__":
-    # logging.basicConfig(filename="./flask_error.log", level=logging.DEBUG)
-    app.run(load_dotenv=True,debug=True)
+    app.run()
